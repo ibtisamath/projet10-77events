914f9d5e63f2eb9a4b5e9fd306ca491156412ee7
ibtisamath <ibtisamathsoumanou1@gmail.com>
2024-04-17 14:09:22 +0200

    key

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -51,7 +51,7 @@ const Slider = () => {
         <div className="SlideCard__pagination">
           {byDateDesc?.map((event, radioIdx) => (
             <input
-              key={`${event.id}`}
+              key={`${event.title}`}
               type="radio"
               name="radio-button"
               checked={index === radioIdx}
